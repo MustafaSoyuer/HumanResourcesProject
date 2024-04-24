c41f4c069bb380e9fe19207a196f8944c8849381
umitgecekus <umit.gecekus1905@gmail.com>
2024-04-24 17:09:58 +0300

    deneme100

--- src/main/java/com/project/HumanResourcesProjectApplication.java
+++ src/main/java/com/project/HumanResourcesProjectApplication.java
@@ -15,6 +15,9 @@ public class HumanResourcesProjectApplication {
 		System.out.println("denemeeeeeeeeeeee");
 		System.out.println("denemeeeeeeeeeeee");
 
+		System.out.println("denemeeeeeeeeeeee");
+		System.out.println("denemeeeeeeeeeeee");
+
 	}
 
 }
