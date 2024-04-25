9db75854fafaf8aad68879d7951c998af60b87ab
Burcu Sekmen Demir <burcusekmen6@gmail.com>
2024-04-26 01:05:27 +0300

    AdminService ve CompanyService oluşturuldu, veritabanları ile entegrasyonları sağlandı.

---
+++ admin-service/src/main/java/com/project/entity/BaseEntity.java
@@ -0,0 +1,21 @@
+package com.project.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Data
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class BaseEntity {
+
+    private String createdAt;
+    private String updateAt;
+    private boolean state;
+
+
+}
---
+++ admin-service/src/main/java/com/project/service/AdminService.java
@@ -0,0 +1,12 @@
+package com.project.service;
+
+import com.project.repository.AdminRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AdminService {
+
+    private final AdminRepository adminRepository;
+}
