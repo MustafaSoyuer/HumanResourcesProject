5848f6789c84b69a0085d8ba9be03d9e53d79563
Burcu Sekmen Demir <burcusekmen6@gmail.com>
2024-04-25 10:57:59 +0300

    Git Komutları Güncellendi

--- src/main/java/com/project/HumanResourcesProjectApplication.java
+++ src/main/java/com/project/HumanResourcesProjectApplication.java
@@ -8,7 +8,7 @@ public class HumanResourcesProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HumanResourcesProjectApplication.class, args);
-		System.out.println("Merhaba");
+
 
 
 
