4af74d52f87021f73ff44c67940c8d915d85d10e
umitgecekus <umit.gecekus1905@gmail.com>
2024-04-25 23:46:17 +0300

    asdsadsadsa

---
+++ manager-service/src/main/java/com/project/constants/RestApiUrls.java
@@ -0,0 +1,10 @@
+package com.project.constants;
+
+public class RestApiUrls {
+    private static final String DEV = "/dev";
+    private static final String VERSION = "/v1";
+    private static final String ROOT = DEV + VERSION;
+    public static final String MANAGER = ROOT + "/manager";
+
+
+}
