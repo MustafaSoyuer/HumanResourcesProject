b73cbcbf4a1fa62c7eccb51c73edcf161783651e
Effy22 <elif.gur@bilgeadamboost.com>
2024-05-10 14:35:40 +0300

    OpenFeign silindi

--- employee-service/src/main/java/com/project/EmployeeServiceApplication.java
+++ employee-service/src/main/java/com/project/EmployeeServiceApplication.java
@@ -2,11 +2,8 @@ package com.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-
 
 @SpringBootApplication
-@EnableFeignClients
 public class EmployeeServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(EmployeeServiceApplication.class, args);
