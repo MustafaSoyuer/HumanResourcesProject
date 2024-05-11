eb9fce5bd93f46dd48dad9ca252002b5e36c45ba
Effy22 <elif.gur@bilgeadamboost.com>
2024-05-11 15:48:05 +0300

    Approve ve reject metotlarına repository kaydı eklendi.

---
+++ requirements-service/src/main/java/com/project/controller/EquipmentsController/EquipmentsController.java
@@ -0,0 +1,7 @@
+package com.project.controller.EquipmentsController;
+
+public class EquipmentsController {
+    //zimmet sayfası için add, getAll, iadeEt gibi metotlar yazabiliriz. token ile teyitlememiz gerekiyor.
+
+
+}
